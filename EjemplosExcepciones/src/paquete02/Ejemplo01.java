306f506bdb97e2fc35907b10ad1742fde15102e3
jhon pardo <jhonedupardo15@gmail.com>
2026-01-21 12:29:17 -0500

    Clase

---
+++ EjemplosExcepciones/src/paquete02/Ejemplo01.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+import java.util.Scanner;
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int valor1=  ingresarValor();
+        int valor2=  ingresarValor();
+        int sumaTotal= obtenerSuma(valor1, valor2);
+       
+       }
+
+    public static int ingresarValor() {
+        Scanner entrada = new Scanner(System.in);
+        int num1;
+        
+        System.out.println("Ingrese el numero tipo entero");
+        num1 = entrada.nextInt();
+         
+        return num1;
+    }
+
+    public static void imprmir  {
+
+    }
+
+    public static int obtenerSuma() {
+      
+    }
+
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
